e413956e00c800eb61930e2b8f11bd7a32a9a5d7
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-01-28 09:04:55 +0100

    taak1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$student = array ("Abdelrahman", "Mohamed", "LCTSDO1C", "23 jaar");
+echo "<li>$student[0]</li><li>$student[1]</li><li>$student[2]</li><li>$student[3]</li>";
+?>
+
+</body>
+</html>
