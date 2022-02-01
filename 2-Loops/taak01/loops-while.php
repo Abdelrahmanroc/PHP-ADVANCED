2d50d665d76b2f914794eb156f2c23647ccc4c09
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-02-01 12:04:20 +0100

    loop

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$leeftijd = 0;
+
+while($leeftijd < 18){
+
+    echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen.";
+    echo"<br>";
+    $leeftijd++;
+
+}
+
+echo"Ik ben $leeftijd, dus ik heb stemrecht!";
+
+?>
