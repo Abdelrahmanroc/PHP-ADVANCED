3cebc54b84ed6c4a5d713607414b2193c6d148c3
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-02-03 11:18:33 +0100

    verzend opdracht taak 4

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<?php
-
-<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-<label for="vehicle1"> I have a bike</label><br>
-<input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-<label for="vehicle2"> I have a car</label><br>
-<input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
-<label for="vehicle3"> I have a boat</label><br>
-?>
+<center>
+<form action="verzend.php">
+    <label for="klas">ik zit in klas:</label>
+<select>
+    <?php 
+    $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+    foreach ($klassen as $klas) {
+     ?>
+    <option><?php echo $klas ?> </option>
+    <?php } ?>
+ </select>
+  <input type="submit" name="submit" value="verzenden">  
+</form>
+</center>
 </body>
 </html>
---
+++ 2-Loops/taak04/verzend.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<?php 
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+
+foreach($klassen as $value){
+    echo"<ul><li>$value</li></ul>";
+}
+?>;
+
+<?php echo "Succes met je opleiding " ?>;
+
+</body>
+</html>
