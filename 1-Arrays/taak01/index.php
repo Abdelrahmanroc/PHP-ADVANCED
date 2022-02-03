1296b03b282de5e8991bcde0465fe9ce5ec5b758
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-02-03 10:50:00 +0100

    yes

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -6,14 +6,19 @@
 $student = array ("voornaam"=>"Abdelrahman","achternaam"=>"Mohamed","Klas"=>"LCTSDO1C","leeftijd"=>"23");
 echo "gegevens Abdelrahman Mohamed:";
 echo "<br>";
+
 echo "voornaam :" . $student["voornaam"];
 echo "<br>";
+
 echo "achternaam :" . $student["achternaam"];
 echo "<br>";
+
 echo "Klas :" . $student["Klas"];
 echo "<br>";
+
 echo "leeftijd :" . $student["leeftijd"] ."jaar";
 echo "<br>";
+
 ?>
 
 </body>
