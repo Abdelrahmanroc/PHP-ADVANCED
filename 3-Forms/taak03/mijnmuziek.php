2c30482d885c08b1b78ab87f00fd1b1eaa860f57
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-02-08 19:52:16 +0100

    finished

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<h3>Gegevens van beste rapper terwerld! </h3>
+<p>klick op het link </p>
+
+<form name id="muziek" action = "mijngegevens.php" method="get">
+
+
+<a href="mijngegevens.php?
+artiest=Drake&
+titel=Beste rapper&
+JaarVanUitkomst=2010&
+muzieksoort=Rap">versturen</a>
+
+</body>
+</html>
